da370491a49f63846dc5cbb2d6c2bb21abaab3ed
timcop <timothy.copland98@gmail.com>
2021-08-28 16:11:12 +1200

    Changed sql statement so add working now

--- www/insert_product.php
+++ www/insert_product.php
@@ -6,11 +6,12 @@
 
     $product_name = $_REQUEST['name'];
     $product_type = $_REQUEST['type'];
-    $desired_quantity = $_REQUEST['dq'];
+    $desired_quantity = $_REQUEST['dq'] + 0;
 
     $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+    echo "Performed: INSERT INTO Products VALUES ('$product_name', '$product_type', $desired_quantity)";
 
     $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-    $sql = "INSERT INTO Products (name, type) VALUES ('" . $product_name . "','" . $product_type . "','" . $desired_quantity . "')";
+    $sql = "INSERT INTO Products VALUES ('$product_name', '$product_type', $desired_quantity)";
     $pdo->exec($sql)
 ?>
--- www/test.php
+++ www/test.php
@@ -113,6 +113,5 @@
                 </fieldset>
             </form>
         </section>
-        
     </body>
 </html>
