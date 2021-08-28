7aeaecec1e44b786c4d50dde1e74588983c4abde
timcop <timothy.copland98@gmail.com>
2021-08-28 16:24:11 +1200

    tidied up

--- www/delete_product.php
+++ www/delete_product.php
@@ -8,11 +8,11 @@
     $product_name = $product_to_delete[0];
     $product_type = $product_to_delete[1];
     $desired_quantity = $product_to_delete[2];
-    echo "Performed: DELETE FROM Products WHERE name='$product_name' AND type='$product_type' AND desired_quantity=$desired_quantity";
-
+    
     $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
 
     $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
     $sql = "DELETE FROM Products WHERE name='$product_name' AND type='$product_type' AND desired_quantity=$desired_quantity";
+    echo $sql;
     $pdo->exec($sql)
 ?>
--- www/insert_product.php
+++ www/insert_product.php
@@ -6,12 +6,12 @@
 
     $product_name = $_REQUEST['name'];
     $product_type = $_REQUEST['type'];
-    $desired_quantity = $_REQUEST['dq'] + 0;
+    $desired_quantity = $_REQUEST['dq'];
 
     $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
-    echo "Performed: INSERT INTO Products VALUES ('$product_name', '$product_type', $desired_quantity)";
 
     $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
     $sql = "INSERT INTO Products VALUES ('$product_name', '$product_type', $desired_quantity)";
+    echo $sql;
     $pdo->exec($sql)
 ?>
