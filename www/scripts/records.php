cee411490235bca31e28e3fc8dc4366a15cee820
jordankettles <70495950+jordankettles@users.noreply.github.com>
2021-09-01 19:47:17 +1200

    Update records.php
    Add CSS and reformat

--- www/scripts/records.php
+++ www/scripts/records.php
@@ -1,63 +1,43 @@
 <!DOCTYPE HTML>
 <html lang="en">
     <head>
-        <title>Stocktake Record</title>
-        <style>
-            th { text-align: left; }
-
-            table, th, td, fieldset {
-            border: 2px solid grey;
-            border-collapse: collapse;
-            }
-
-            th, td {
-            padding: 0.2em;
-            }
-
-            fieldset {
-                width: 40%;
-            }
-        </style>
+        <title>Stocktake Record #<?php echo $_REQUEST['id']?></title>
+        <link rel="stylesheet" href="../style.css" type="text/css">
     </head>
 
     <body>
-        <h1>Stocktake Record</h1>
+        <?php
+            $db_host = '127.0.0.1';
+            $db_name = 'stocktake';
+            $db_user = 'root';
+            $db_passwd = 'insecure_mysqlroot_pw';
+            $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+            $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 
-        <h2>Record</h2>
+            $stock_num = $_REQUEST['id'];
+            $q = $pdo->query("SELECT * FROM StocktakeProds WHERE stocktake_num=$stock_num");
+            echo "<h1>Stocktake Record #$stock_num</h1>\n
+                    <table>\n
+                    <tr>\n
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Desired Quantity</th>
+                    <th>Current Quantity</th>
+                    </tr>";
 
-        <table border='1'>
-            <tr><th>Name</th><th>Type</th><th>Derired quantity</th><th>Current quantity</th><th>Stocktake #</th></tr>
-
-            <?php
-                $db_host = '127.0.0.1';
-                $db_name = 'stocktake';
-                $db_user = 'root';
-                $db_passwd = 'insecure_mysqlroot_pw';
-                $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
-                $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-
-                $stock_num = $_REQUEST['id'];
-                $q = $pdo->query("SELECT * FROM StocktakeProds WHERE stocktake_num=$stock_num");
-    
-                while ($row = $q->fetch()) {
-                    echo '<tr><td>';
-                    echo $row['name'];
-                    echo '</td><td>';
-                    echo $row['type'];
-                    echo '</td><td>';
-                    echo $row['desired_quantity'];
-                    echo '</td><td>';
-                    if ($row["current_quantityInt"] == NULL) {
-                        echo $row["current_quantityDec"];
-                    } else {
-                        echo $row["current_quantityInt"];
-                    }
-                    echo '</td><td>';
-                    echo $row['stocktake_num'];
-                    echo '</td><td>';
-                }
-            ?>
-        </table>
+            while ($row = $q->fetch()) {
+                echo '<tr><td>';
+                echo $row['name'];
+                echo '</td><td>';
+                echo $row['type'];
+                echo '</td><td>';
+                echo $row['desired_quantity'];
+                echo '</td><td>';
+                echo $row["current_quantity"];
+                echo '</td></tr>';
+            }
+            echo "</table>\n";
+        ?>
     </body>
 </html>
 
